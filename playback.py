ba8a3c377f1469009ae042cc02165762e5019a98
twoshift8 <205802733+twoshift8@users.noreply.github.com>
2025-06-26 18:50:46 -0400

    Delete playback.py

--- playback.py
+++
@@ -1,8 +0,0 @@
-def main():
-    hello = input("Hi... I'm... Sam... the... Sloth... \nWhat's... your... name..? \n").title()
-    print(f"Hi.., {hello}...")
-    day = input("How's... your... day... been..? \n")
-    reverb = day.replace(' ', '...')
-    print("Oh... did.. you.. say..:", reverb, "? \n Just... Take... a... Nap...\n")
-
-main()
